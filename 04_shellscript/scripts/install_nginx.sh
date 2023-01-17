d0e38be6ad73b9c23fc64d4089e96aeee3d9a21b
Mohit Kumar <mohitkr05@gmail.com>
2023-01-17 22:51:47 +1100

    added code for shell script

--- 04_shellscript/scripts/install_nginx.sh
+++ 04_shellscript/scripts/install_nginx.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-echo "Do nothing"
+# echo "Do nothing"
 
-# echo "Performing yum update and installing epel-release"
-# yum -y update && yum -y install epel-release 
+echo "Performing yum update and installing epel-release"
+yum -y update && yum -y install epel-release 
 
-# echo "Installing NGINX"
-# yum -y install nginx
+echo "Installing NGINX"
+yum -y install nginx
 
-# echo "Enabling NGINX"
-# systemctl enable nginx
+echo "Enabling NGINX"
+systemctl enable nginx
 
-# echo "Starting NGINX"
-# systemctl start nginx
+echo "Starting NGINX"
+systemctl start nginx
 
-# echo "I have configured NGINX"
+echo "I have configured NGINX"
